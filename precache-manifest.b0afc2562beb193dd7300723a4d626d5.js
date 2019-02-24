195e9e6ee56baa86748fe4ce26d97506de059f44
isacoder <isabel.anguianop@gmail.com>
2019-02-24 19:45:18 +0000

    Updates

---
+++ precache-manifest.b0afc2562beb193dd7300723a4d626d5.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "a3ae3d1bd00f11dc74dacb9c6694fd51",
+    "url": "/camera-system/static/media/search.a3ae3d1b.svg"
+  },
+  {
+    "revision": "1b7929c208c38185ae5e",
+    "url": "/camera-system/static/css/main.b91eb923.chunk.css"
+  },
+  {
+    "revision": "e982da6a9e85554cb132",
+    "url": "/camera-system/static/js/runtime~main.e982da6a.js"
+  },
+  {
+    "revision": "66bf866f9c897428f0c0",
+    "url": "/camera-system/static/js/2.66bf866f.chunk.js"
+  },
+  {
+    "revision": "f22ad071eba3a41d20670ece52f6535c",
+    "url": "/camera-system/static/media/logo.f22ad071.svg"
+  },
+  {
+    "revision": "1b7929c208c38185ae5e",
+    "url": "/camera-system/static/js/main.1b7929c2.chunk.js"
+  },
+  {
+    "revision": "fb7edf77d097bdf31355d2066e257cc4",
+    "url": "/camera-system/static/media/sort-amount-up.fb7edf77.svg"
+  },
+  {
+    "revision": "c6f1f90018c5bf4c419b5fb53d61e15d",
+    "url": "/camera-system/static/media/sort-amount-down.c6f1f900.svg"
+  },
+  {
+    "revision": "03bc7b67b66accdd24ec4491fc8d57ed",
+    "url": "/camera-system/static/media/layer-group-slash.03bc7b67.svg"
+  },
+  {
+    "revision": "972803d88e212d8b53669e3141eec846",
+    "url": "/camera-system/static/media/layer-group.972803d8.svg"
+  },
+  {
+    "revision": "db6b4281e96428f9ad8bae9ddc1f5be6",
+    "url": "/camera-system/index.html"
+  }
+];
